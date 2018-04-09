5acc7bba573b9f1e560079bc49853fc366ff3f23
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-09 11:02:52 +0100

    google oauth api

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+//ClientID: 946694402490-es6mgm86d1q2vpgo70gb63oclu070c49.apps.googleusercontent.com
+//Client Secret: VMtl1DRSO87yeKmlD9utALw0
+
+passport.use(new GoogleStrategy());
+
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there' });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
