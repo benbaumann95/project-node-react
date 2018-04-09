2212f60ca6820b90d7a82946e4fedd9a0f0da12f
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-09 17:33:38 +0100

    logout feature

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,13 +8,12 @@ module.exports = app => {
     })
   );
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    function(req, res) {
-      res.redirect('/');
-    }
-  );
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
