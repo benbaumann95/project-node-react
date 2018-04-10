4916f1580edb8506b1083bd9991ca3cd17d5cc32
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-10 10:22:59 +0100

    async function

---
+++ fetch-promise-syntax.js
@@ -0,0 +1,31 @@
+// write a function to retrieve some json
+// make an ajax request
+// use the fetch function
+
+// function fetchAlbums(url) {
+//   fetch(url)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(myJson) {
+//       console.log(myJson);
+//     });
+// }
+//
+// fetchAlbums('http://rallycoding.herokuapp.com/api/music_albums');
+
+// function fetchAlbums() {
+//   fetch('http://rallycoding.herokuapp.com/api/music_albums')
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+// }
+//
+// fetchAlbums();
+
+async function fetchAlbums() {
+  // await in front of anything that gives a promise
+  const res = await fetch('http://rallycoding.herokuapp.com/api/music_albums');
+  const json = await res.json();
+
+  console.log(json);
+}
