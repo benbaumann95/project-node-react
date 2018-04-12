dcea3802f0413f6362848fe895ee483c059f784e
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-12 19:10:19 +0100

    Payments component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,8 +16,10 @@ class Header extends React.Component {
         );
       default:
         return [
-          <li>Payments</li>,
-          <li>
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
           </li>
         ];
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,13 +3,16 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends React.Component {
   render() {
-    debugger;
     return (
       <StripeCheckout
+        name="QuickSurvey"
+        description="add email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn blue">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
