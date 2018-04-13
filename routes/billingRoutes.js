a690492007ad7b79c26ff775ecf9b7beb4798021
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-13 11:33:26 +0100

    completed stripe billing and updating credits in navbar

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,3 +1,24 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
+
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {});
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    // if (!req.user) {
+    //   return res
+    //     .status(401)
+    //     .send({ error: 'You must log in before adding credits' });
+    // }
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$1 per credit',
+      source: req.body.id
+    });
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
+  });
 };
