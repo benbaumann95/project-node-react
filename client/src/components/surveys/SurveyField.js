adbe79f57e15ca9fc5f18778207ddbe483048ecf
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-16 16:40:19 +0100

    survey field component

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logivc to render a single label and text input
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
