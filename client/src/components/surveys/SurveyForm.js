d2ffc38c5fe394d2e41fe0bfcf6d77121566d079
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-16 16:18:52 +0100

    redux-form and form components

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,10 @@
+// SurveyForm shows a form for a user to add input
+import React from 'react';
+
+class SurveyForm extends React.Component {
+  render() {
+    return <div>SurveyForm</div>;
+  }
+}
+
+export default SurveyForm;
